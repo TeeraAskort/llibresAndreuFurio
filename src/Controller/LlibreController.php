dbb7ef6a89f3a54d054eda906a21a27a1ed37117
Alderaeney <alderaeney@gmail.com>
2022-02-15 09:26:21 +0100

    Commit Doctrine LlibresAndreuFurio

--- src/Controller/LlibreController.php
+++ src/Controller/LlibreController.php
@@ -2,16 +2,109 @@
 
 namespace App\Controller;
 
+use App\Entity\Editorial;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Llibre;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
 
 class LlibreController extends AbstractController
 {
     private $llibres;
+    private $dades;
 
     public function __construct($dades)
     {
+        $this->dades = $dades;
         $this->llibres = $dades->getLlibres();
+    }
+
+    /**
+     * @Route("/llibre/inserir", name="inserir")
+     */
+    public function inserir(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+
+        $llibre = new Llibre();
+        $llibre->setIsbn('7777SSSS');
+        $llibre->setTitol('Noruega');
+        $llibre->setAutor('Rafa Lahuerta');
+        $llibre->setPagines(387);
+        $llibre->setImatge('llibre.png');
+        $entityManager->persist($llibre);
+        // $entityManager->flush();
+        // return new Response("Llibre inserit amb isbn " . $llibre->getIsbn());
+        $dorohedoro = new Llibre();
+        $dorohedoro->setIsbn('1111AAA');
+        $dorohedoro->setTitol('Dorohedoro');
+        $dorohedoro->setAutor('Q-Hayashida');
+        $dorohedoro->setPagines(170);
+        $dorohedoro->setImatge('dorohedoro.jpg');
+        $entityManager->persist($dorohedoro);
+
+        $boys = new Llibre();
+        $boys->setIsbn('2222BBB');
+        $boys->setTitol("20th Century Boys");
+        $boys->setAutor('Naoki Urasawa');
+        $boys->setPagines('208');
+        $boys->setImatge('boys.jpg');
+        $entityManager->persist($boys);
+
+        $chunyan = new Llibre();
+        $chunyan->setIsbn('3333CCC');
+        $chunyan->setTitol('Chunyan, La Nueva Leyenda');
+        $chunyan->setAutor('CLAMP');
+        $chunyan->setPagines('224');
+        $chunyan->setImatge('chunyan.jpg');
+        $entityManager->persist($chunyan);
+
+        try {
+            $entityManager->flush();
+            return new Response("Inserits llibres amb isbn " . $dorohedoro->getIsbn() . " " . $boys->getIsbn() . " " . $chunyan->getIsbn());
+        } catch (\Exception $e) {
+            return new Response("Error inserint llibres amb isbn " . $dorohedoro->getIsbn() . " " . $boys->getIsbn() . " " . $chunyan->getIsbn());
+        }
+    }
+
+    /**
+     * @Route("/llibre/pagines/{pagines}", name="filtrar_pagines")
+     */
+    public function filtrar_pagines($pagines)
+    {
+        return $this->render('llista-major.html.twig', array('llibres' => $this->dades->getLlibresGreaterThanPagines($pagines), 'pagines' => $pagines));
+    }
+
+    /**
+     * @Route("/llibre/inserirambeditorial", name="inserirambeditorial")
+     */
+    public function inserir_amb_editorial(ManagerRegistry $doctrine)
+    {
+        $EdRepository = $doctrine->getRepository(Editorial::class);
+        $entityManager = $doctrine->getManager();
+        $editorial = $EdRepository->findOneBy(['nom' => 'Bromera']);
+        if ($editorial == null) {
+            $editorial = new Editorial();
+            $editorial->setNom("Bromera");
+            $entityManager->persist($editorial);
+            $entityManager->flush();
+            $editorial = $EdRepository->findOneBy(['nom' => 'Bromera']);
+        }
+        $llibre = new Llibre();
+        $llibre->setIsbn('8888TTT');
+        $llibre->setTitol('Nuestra Salvaje Juventud');
+        $llibre->setPagines(200);
+        $llibre->setImatge('salvaje.jpg');
+        $llibre->setAutor('Mari Okada, Nao Emoto');
+        $llibre->setEditorial($editorial);
+        $entityManager->persist($llibre);
+        try {
+            $entityManager->flush();
+            return new Response("Llibre amb editorial inserit correctament, isbn: " . $llibre->getIsbn());
+        } catch (\Exception $e) {
+            return new Response("Error inserint llibre amb editorial, isbn: " . $llibre->getIsbn());
+        }
     }
 
     /**
@@ -22,7 +115,7 @@ class LlibreController extends AbstractController
 
         $llibre = null;
         foreach ($this->llibres as $book) {
-            if ($book['isbn'] == $isbn) {
+            if ($book->getIsbn() == $isbn) {
                 $llibre = $book;
             }
         }
--- src/Service/BDProvaLlibres.php
+++ src/Service/BDProvaLlibres.php
@@ -2,17 +2,26 @@
 
 namespace App\Service;
 
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Llibre;
+use App\Repository\LlibreRepository;
+
 class BDProvaLlibres
 {
-    private $llibres = array(
-        array('isbn' => "A111B3", 'titol' => "El joc d'Ender", 'autor' => "Orson Scott Card", "pagines" => 350, 'imatge' => "ender.jpg"),
-        array('isbn' => "16703-55-5", 'titol' => "Qué torpe eres, ueno", 'autor' => "tugeneko", "pagines" => 200, 'imatge' => "ueno.jpg"),
-        array('isbn' => "18419-16-4", 'titol' => "acabé hecha un trapo huyendo de la realidad", 'autor' => "kabi nagata", "pagines" => 200, 'imatge' => "trapo.jpg"),
-        array('isbn' => "18222-20-7", 'titol' => "Rastros de sangre 2", 'autor' => "Shuzo Oshimi", "pagines" => 200, 'imatge' => "sangre.jpg"),
-    );
+    private $repository;
+
+    public function __construct(LlibreRepository $repository)
+    {
+        $this->repository = $repository;
+    }
 
     function getLlibres()
     {
-        return $this->llibres;
+        return $this->repository->findAll();
+    }
+
+    function getLlibresGreaterThanPagines($pagines)
+    {
+        return $this->repository->findByPaginesGreaterThan($pagines);
     }
 }
